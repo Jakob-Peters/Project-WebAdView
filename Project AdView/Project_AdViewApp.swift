0e376cf1b9692d94b7af3fab69aa6895591e8491
Jakob Peters <jaspe@stepentwork.dk>
2025-07-21 09:54:52 +0200

    Initial Commit

---
+++ Project AdView/Project_AdViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_AdViewApp.swift
+//  Project AdView
+//
+//  Created by Jakob Svanborg Peters on 21/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Project_AdViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
